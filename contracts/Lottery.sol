71d1ab63ef11ba963307bca058dc2564ab921979
acidrums <edoardo.carradori.96@gmail.com>
2023-03-03 20:24:22 +0100

    Lottery.sol Setup

---
+++ contracts/Lottery.sol
@@ -0,0 +1,37 @@
+// Lottery
+// Enter the lottery (paying some amount)
+// Winner to be selected every X minutes -> completely automated
+// Chainlink Oracle -> Randomness, Automated Execution (Chainlink Keepers)
+
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.18;
+
+error Lottery__NotEnoughETHEntered();
+
+contract Lottery {
+    /* State Variables */
+    uint256 private immutable i_entranceFee;
+    address payable[] private s_players;
+
+    constructor(uint256 entranceFee) {
+        i_entranceFee = entranceFee;
+    }
+
+    function enterLottery() public payable {
+        // require(msg.value > i_entranceFee, "Not enough ETH!")
+        if(msg.value < i_entranceFee) {
+            revert Lottery__NotEnoughETHEntered(); 
+        }
+        s_players.push(payable(msg.sender));
+    }
+
+    function pickRandomWinner() {}
+
+    function getEntranceFee() public view returns(uint256) {
+        return i_entranceFee;
+    }
+
+    function getPlayers(uint256 index) public view returns(address) {
+        return s_players[index];
+    }
+}

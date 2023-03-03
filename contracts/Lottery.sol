2febff0c648614836b531384b0b207a330885067
acidrums <edoardo.carradori.96@gmail.com>
2023-03-03 20:45:35 +0100

    Events in Lottery.sol

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -13,25 +13,31 @@ contract Lottery {
     uint256 private immutable i_entranceFee;
     address payable[] private s_players;
 
+    /* Events */
+    event LotteryEnter(address indexed player)
+
     constructor(uint256 entranceFee) {
         i_entranceFee = entranceFee;
     }
 
     function enterLottery() public payable {
         // require(msg.value > i_entranceFee, "Not enough ETH!")
-        if(msg.value < i_entranceFee) {
-            revert Lottery__NotEnoughETHEntered(); 
+        if (msg.value < i_entranceFee) {
+            revert Lottery__NotEnoughETHEntered();
         }
         s_players.push(payable(msg.sender));
+        // Emit an event when we update a dynamic array or mapping
+        // Named events with the function name reversed
+        emit LotteryEnter(msg.sender); 
     }
 
     function pickRandomWinner() {}
 
-    function getEntranceFee() public view returns(uint256) {
+    function getEntranceFee() public view returns (uint256) {
         return i_entranceFee;
     }
 
-    function getPlayers(uint256 index) public view returns(address) {
+    function getPlayers(uint256 index) public view returns (address) {
         return s_players[index];
     }
 }

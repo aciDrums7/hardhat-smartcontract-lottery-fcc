4a661607ca1210ebf32766a13acd024fa85107b8
acidrums <edoardo.carradori.96@gmail.com>
2023-03-06 16:53:05 +0100

    Implementing Chainlink VRF (Introduction)

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -6,17 +6,19 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.18;
 
+import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
+
 error Lottery__NotEnoughETHEntered();
 
-contract Lottery {
+contract Lottery is VRFConsumerBaseV2 {
     /* State Variables */
     uint256 private immutable i_entranceFee;
     address payable[] private s_players;
 
     /* Events */
-    event LotteryEnter(address indexed player)
+    event LotteryEnter(address indexed player);
 
-    constructor(uint256 entranceFee) {
+    constructor(address vrfCoordinatorV2, uint256 entranceFee) VRFConsumerBaseV2(vrfCoordinatorV2) {
         i_entranceFee = entranceFee;
     }
 
@@ -28,11 +30,21 @@ contract Lottery {
         s_players.push(payable(msg.sender));
         // Emit an event when we update a dynamic array or mapping
         // Named events with the function name reversed
-        emit LotteryEnter(msg.sender); 
+        emit LotteryEnter(msg.sender);
     }
 
-    function pickRandomWinner() {}
+    function requestRandomWinner() external {
+        // Request the random number
+        // One we get it, do something with it
+        // 2 transaction process
+    }
 
+    function fulfillRandomWords(
+        uint256 requestId,
+        uint256[] memory randomWords
+    ) internal override {}
+
+    /* View / Pure functions */
     function getEntranceFee() public view returns (uint256) {
         return i_entranceFee;
     }

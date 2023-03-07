5141e6844b9f51c7681c805e5a0b64d2bcde8b58
acidrums <edoardo.carradori.96@gmail.com>
2023-03-07 15:30:01 +0100

    Deploying Raffle.sol (Mock Chainlink VRF Coordinator)

--- deploy/00-deploy-mocks.js
+++ deploy/00-deploy-mocks.js
@@ -1,13 +1,30 @@
-const { network } = require("hardhat")
+const { network, ethers } = require("hardhat")
 const { developmentChains } = require("../helper-hardhat-config")
+
+// 0.25 is the "premium": it costs 0.25 LINK per request
+const BASE_FEE = ethers.utils.parseETH("0.25")
+// calculated value based on the gas price of the chain
+// Link per gas
+const GAS_PRICE_LINK = 1e9
+
+// ETH price 1,000,000,000
+// Chainlink Nodes pay the gas fees to give us randomness & to do external execution
+// So the price of requests change based on the price of gas
 
 module.exports = async ({ getNamedAccounts, deployments }) => {
     const { deploy, log } = deployments
     const { deployer } = await getNamedAccounts()
-    const chainId = network.config.chainId
+    const args = [BASE_FEE, GAS_PRICE_LINK]
 
     if (developmentChains.includes(network.name)) {
         log("Local network detected! Deploying mocks...")
         // deploy a mock vrfCoordinatorV2...
+        await deploy("VRFCoordinatorV2Mock", {
+            from: deployer,
+            log: true,
+            args: args
+        })
+        log("Mocks deployed!")
+        log("--------------------------------------------------------------------------------")
     }
 }
--- deploy/01-deploy-lottery.js
+++ deploy/01-deploy-lottery.js
@@ -1,14 +1,20 @@
 const { network } = require("hardhat")
+const { developmentChains } = require("../helper-hardhat-config")
 
 export default async ({ getNamedAccounts, deployments }) => {
     const { deploy, log } = deployments
     const { deployer } = await getNamedAccounts()
+    let vrfCoordinatorV2Address
 
+    if (developmentChains.includes(network.name)) {
+        const vrfCoordinatorV2Mock = await ethers.getContract("VRFCoordinatorV2Mock")
+        vrfCoordinatorV2Address = vrfCoordinatorV2Mock.address
+    }
 
-
+    const args = []
     const lottery = await deploy("Lottery", {
         from: deployer,
-        args: [],
+        args: args,
         log: true,
         waitConfirmations: network.config.blockConfirmations || 1
     })
